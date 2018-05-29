6d8aae1c53ece7d2b8625320417f63dbea2242dd
esmaeelmoradi <esmaeel.moradi@gmail.com>
2018-05-28 20:31:05 -0500

    assignment2 code is updated to the final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(myMatrix = matrix()){
+	#set a null value for the inverse value
+	inv.matrix <- NULL
+	
+	#set the value of myMatrix and reset the inverse value
+	set <- function(A){
+		myMatrix <<- A
+		inv.matrix <<- NULL
+	}
+	
+	#check the matrix
+	get <- function() myMatrix
+	
+	#set the inverse value
+	setInv <- function(inverse) inv.matrix <<- inverse
+	
+	#check the inverse value
+	getInv <- function() inv.matrix
+	
+	list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##This function computes the inverse of the special "matrix"
+cacheSolve <- function(myMatrix, ...){
+	#checking the existing inverse value
+	inv.matrix <- myMatrix$getInv()
+	
+	if(!is.null(inv.matrix)){
+		message("getting cached data")
+		return(inv.matrix)
+	}
+	
+	mtrx <- myMatrix$get()
+	
+	#the matrix should be assigned in some time
+	if(is.na(mtrx)){
+		message("The matrix inside myMatrix has not been set yet!")
+		return()
+	}
+	#matrix needs to be inversable
+	else if(nrow(mtrx) != ncol(mtrx) || det(mtrx) == 0){
+		message("The matrix is not inversable!")
+		return()
+	}
+	#calculate the inverse and set the value for that
+	else{
+		inv.matrix <- solve(mtrx)
+		myMatrix$setInv(inv.matrix)
+		return(inv.matrix)
+	}
 }
